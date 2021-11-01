ef928465b370f4477e68638a3badb5df6b9930a4
a <a@aui-MacBookAir.local>
2021-11-01 15:09:31 +0900

    댓글 수정 삭제

--- routes/comments.js
+++ routes/comments.js
@@ -1,36 +1,73 @@
-// routes/comments.js
-
 var express  = require('express');
 var router = express.Router();
 var Comment = require('../models/Comment');
 var Post = require('../models/Post');
-var User = require('../models/User');
 var util = require('../util');
 
 // create
-router.post('/', util.isLoggedin, checkPostId, function(req, res){ // 1
-  var post = res.locals.post; // 1-1
+router.post('/', util.isLoggedin, checkPostId, function(req, res){
+  var post = res.locals.post;
 
-  req.body.author = req.user._id; // 2
-  req.body.post = post._id;       // 2
+  req.body.author = req.user._id;
+  req.body.post = post._id;
 
   Comment.create(req.body, function(err, comment){
     if(err){
-      req.flash('commentForm', { _id: null, form:req.body });                 // 3
-      req.flash('commentError', { _id: null, errors:util.parseError(err) });  // 3
+      req.flash('commentForm', { _id:null, form:req.body });
+      req.flash('commentError', { _id:null, errors:util.parseError(err) });
     }
-    return res.redirect('/posts/'+post._id+res.locals.getPostQueryString()); //4
+    return res.redirect('/posts/'+post._id+res.locals.getPostQueryString());
+  });
+});
+
+// update
+router.put('/:id', util.isLoggedin, checkPermission, checkPostId, function(req, res){
+  var post = res.locals.post;
+
+  req.body.updatedAt = Date.now();
+  Comment.findOneAndUpdate({_id:req.params.id}, req.body, {runValidators:true}, function(err, comment){
+    if(err){
+      req.flash('commentForm', { _id:req.params.id, form:req.body });
+      req.flash('commentError', { _id:req.params.id, errors:util.parseError(err) });
+    }
+    return res.redirect('/posts/'+post._id+res.locals.getPostQueryString());
+  });
+});
+
+// destroy
+router.delete('/:id', util.isLoggedin, checkPermission, checkPostId, function(req, res){
+  var post = res.locals.post;
+
+  Comment.findOne({_id:req.params.id}, function(err, comment){
+    if(err) return res.json(err);
+
+    // save updated comment
+    comment.isDeleted = true;
+    comment.save(function(err, comment){
+      if(err) return res.json(err);
+
+      return res.redirect('/posts/'+post._id+res.locals.getPostQueryString());
+    });
   });
 });
 
 module.exports = router;
 
 // private functions
-function checkPostId(req, res, next){ // 1
-  Post.findOne({_id:req.query.postId},function(err, post){
+function checkPermission(req, res, next){
+  Comment.findOne({_id:req.params.id}, function(err, comment){
+    if(err) return res.json(err);
+    if(comment.author != req.user.id) return util.noPermission(req, res);
+
+    next();
+  });
+}
+
+function checkPostId(req, res, next){
+  Post.findOne({_id:req.query.postId}, function(err, post){
     if(err) return res.json(err);
 
-    res.locals.post = post; // 1-1
+    res.locals.post = post;
     next();
   });
 }
