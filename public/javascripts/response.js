e4e0c36a972c83bc7cc467029bd8f20dbef3e463
seohl16 <selim@student.42seoul.kr>
2021-11-04 18:00:38 +0900

    navbar login alim(알림) class added

--- public/javascripts/response.js
+++ public/javascripts/response.js
@@ -5,13 +5,28 @@ jQuery(document).ready(function($) {
 		$('#response').removeClass('btn-group-vertical');
 		$('#response').addClass('btn-group');
 		$('#response').addClass('d-flex');
+		// $('#navbar_alim').removeClass('d-flex');
+		var alim = document.getElementById('navbar_alim');
+		if (alim.hasChildNodes()){
+			alim.removeChild(alim.lastChild);
+		}
 		$('#lg-row').removeClass('row');
 	  } else if (ww >= 992) {
 		$('#response').removeClass('btn-group');
 		$('#response').removeClass('d-flex');
 		$('#response').addClass('btn-group-vertical');
 		$('#lg-row').addClass('row');
+		var alim = document.getElementById('navbar_alim');
+		if (!alim.hasChildNodes()){
+			let el = document.createElement("h7");
+			let itemStr = `
+			즐겨찾기 기능은 로그인 후 이용가능합니다. `;
 
+			el.innerHTML = itemStr;
+			el.className = "d-flex justify-content-between ml-auto"; 
+
+			alim.appendChild(el);
+		}
 	  };
 	};
 	$(window).resize(function(){
